d19ac4e17925899d5255871b41a1bee827f5db5b
Neelesh Maharjan <neeleshmrz@gmail.com>
2024-03-30 15:29:25 -0400

    feat: logger added

---
+++ QuotesBE/QuotesBE/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using Contracts;
+using LoggerService;
+
+namespace QuotesBE.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+    services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin().
+                WithExposedHeaders("X-Pagination").
+                AllowAnyMethod().
+                AllowAnyHeader());
+    });
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services) => 
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
